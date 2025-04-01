010228d8a18a3d51b6b1fede41b7c9378516fe42
emesierra22 <emesierra9@gmail.com>
2025-04-01 14:22:27 -0500

    Subiendo archivos al nuevo repositorio

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APP_WEB_DE_CREDITO.Startup))]
+namespace APP_WEB_DE_CREDITO
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
